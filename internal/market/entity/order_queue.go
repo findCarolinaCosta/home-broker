637e7ae108fa60ac3a982037491b7718ed89724f
findCarolinaCosta <carolinadacosta1997@gmail.com>
2023-06-24 16:42:28 -0300

    feat: OrderQueue entity

---
+++ internal/market/entity/order_queue.go
@@ -0,0 +1,35 @@
+package entity
+
+type OrderQueue struct {
+	Orders []*Order
+}
+
+func (oq *OrderQueue) Less(i, j int) bool {
+	return oq.Orders[i].Price < oq.Orders[j].Price
+}
+
+func (oq *OrderQueue) Swap(i, j int) {
+	oq.Orders[i], oq.Orders[j] = oq.Orders[j], oq.Orders[i]
+}
+
+func (oq *OrderQueue) Len(i, j int) int {
+	return len(oq.Orders)
+}
+
+func (oq *OrderQueue) Push(x interface{}) {
+	oq.Orders = append(oq.Orders, x.(*Order))
+}
+
+func (oq *OrderQueue) Pop() interface{} {
+	old := oq.Orders
+	len := len(old) - 1
+	newItem := old[len]
+
+	oq.Orders = old[0:len]
+
+	return newItem
+}
+
+func NewOrderQueue() *OrderQueue {
+	return &OrderQueue{}
+}
