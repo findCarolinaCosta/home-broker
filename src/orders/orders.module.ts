326dd88f42ec108b8fc00fa1df4878267361e06f
findCarolinaCosta <carolinadacosta1997@gmail.com>
2023-06-27 20:34:08 -0300

    feat: init config order publisher in kafka

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDER_PUBLISHER',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'orders',
+            brokers: ['host.docker.internal:9094'],
+            // brokers: ['kafka:29092'], // docker internal
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
 })
 export class OrdersModule {}
